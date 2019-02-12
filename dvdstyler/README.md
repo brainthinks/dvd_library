267346bf92773ba60ad75486e57249666fe52626
Brian Andress <opensourcefiend@gmail.com>
2019-02-11 23:16:18 -0500

    update the config signature

--- dvdstyler/README.md
+++ dvdstyler/README.md
@@ -1,6 +1,6 @@
 # Dvd Styler Template Stuff
 
-This is still a work in progress.  I plan on using a source dvdstyler template and filling it with the necessary library items I need.  I am making a lot of assumptions here, so if you want to use this for your own video library, you will need to be sure that you understand what is going on with the template and the template filling logic.  I'll do my best to document what is going, so if there is something missing, submit a pull request for my documentation.
+This is still a work in progress.  I plan on using a source dvdstyler template and filling it with the necessary library items I need.  I am making a lot of assumptions here, so if you want to use this for your own video library, you will need to be sure that you understand what is going on with the template and the template filling logic.  I'll do my best to document what is going, so if there is something missing, submit a pull request for documentation.
 
 ## System Requirements
 
@@ -38,7 +38,7 @@ This is still a work in progress.  I plan on using a source dvdstyler template a
 ## @todo
 
 * docker
-* properly encode videos (ffmpeg?) - currently, it seems like I was able to do this, but they are written to disk wrong :(
+* properly encode videos (ffmpeg?) - currently, it seems like I was able to do this, but they are written to disc wrong :(
 * allow navigating backwards in playall
 * generate assets without using dvdstylers, just use the stock dvdauthor tools
 * edit xcf files from the command line
--- dvdstyler/scripts/generateCatalog.js
+++ dvdstyler/scripts/generateCatalog.js
@@ -5,37 +5,42 @@ const debug = require('debug')('dvdstyler:scripts:generateCatalog');
 const path = require('path');
 const XLSX = require('xlsx');
 
-const config = require('../library/config');
-const library = require('../library/');
+const configs = require('../configs/config.all');
+const library = require('../configs/')(configs);
 
 async function main () {
   const workbook = XLSX.utils.book_new();
 
   const rows = [];
-  const titles = library.byId();
 
-  for (let id in titles) {
-    debug(`Processing ${id}`);
+  for (let i = 0; i < library.discs.length; i++) {
+    const disc = library.discs[i];
 
-    const title = titles[id];
+    for (let j = 0; j < disc.videos.length; j++) {
+      const video = disc.videos[j];
+      const id = video.id;
 
-    rows.push({
-      'Code': title.id,
-      'Is Clone Of': title.parentId || '',
-      'Title': title.title,
-      'Year': title.productionYear || '',
-      'Run Time': title.runTime,
-      'Approx Run Time': `${title.approximateRunTime} min`,
-      'Spanish': title.audio.spanish ? 'Y' : '',
-      'Category': title.category,
-    });
+      debug(`Processing ${id}`);
+
+      rows.push({
+        'Code': id,
+        'Is Clone Of': video.parentId || '',
+        'Title': video.title,
+        'Year': video.productionYear || '',
+        'Run Time': video.runTime,
+        'Approx Run Time': `${video.approximateRunTime} min`,
+        'Spanish': video.audio.spanish ? 'Y' : '',
+        'Category': video.category,
+        'Notes': video.notes || '',
+      });
+    }
   }
 
   const worksheet = XLSX.utils.json_to_sheet(rows);
 
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Catalog');
 
-  XLSX.writeFile(workbook, path.join(__dirname, '..', 'build', config.catalog.fileName), { type: 'file' });
+  XLSX.writeFile(workbook, path.join(__dirname, '..', 'build', library.catalog.fileName), { type: 'file' });
 }
 
 main()
